153d34c078b7c6711d50a363a4177e65a59e5b5c
yugyeonghoon <dbrudgns0626@naver.com>
2025-06-04 21:11:23 +0900

    객체 생성

--- src/main/java/com/cos/blog/model/Board.java
+++ src/main/java/com/cos/blog/model/Board.java
@@ -1,18 +1,21 @@
 package com.cos.blog.model;
 
 import java.sql.Timestamp;
+import java.util.List;
 
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Lob;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -41,6 +44,9 @@ public class Board {
 	@ManyToOne	//Many = Board, One = User 한명의 유저는 여러개의 게시물 작성 가능 (연관관계)
 	@JoinColumn(name="userId")	//필드명은 userId User 객체니까 user.java의 user 참조
 	private User user;	//DB는 오브젝트를 저장할 수 없다. ForeignKey, 자바는 오브젝트를 저장할 수 있다.
+	
+	@OneToMany(mappedBy = "board", fetch = FetchType.EAGER)	//mappedBy 연관관계의 주인이 아니다(난 FK가 아니다.) DB에 컬럼을 만들지 마세요. 필드값을 mappedBy에 넣어준다.
+	private List<Reply> reply;
 	
 	@CreationTimestamp
 	private Timestamp createDate;
