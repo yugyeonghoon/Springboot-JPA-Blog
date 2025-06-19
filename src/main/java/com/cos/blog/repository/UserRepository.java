9ddc349b2862641e8ba5002f933110e62cdd25d9
yugyeonghoon <dbrudgns0626@naver.com>
2025-06-19 18:33:55 +0900

    약간 수정

--- src/main/java/com/cos/blog/repository/UserRepository.java
+++ src/main/java/com/cos/blog/repository/UserRepository.java
@@ -3,7 +3,6 @@ package com.cos.blog.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import com.cos.blog.model.User;
 
